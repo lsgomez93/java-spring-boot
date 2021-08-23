621178b3d4038f18f85a2e4cad9403d306df137e
lsgomez93 <lsgomez@unimayor.edu.co>
2021-08-23 11:53:36 -0500

    Primer commit proyecto spring boot

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return args->{
+            Student hello = new Student(
+                    "Hello"
+            );
+            repository.save(hello);
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public String getStudents() {
+
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
+}
+
